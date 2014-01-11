a906ff1827b29d2a83139d8d4ae51254e373e919
Daniel Thorpe <dan@blindingskies.com>
2014-01-11 22:30:55 +0000

    [master]: Adds a framework header file.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

---
+++ Classes/DNTCoreData.h
@@ -0,0 +1,15 @@
+//
+//  DNTCoreData.h
+//  DNTCoreData
+//
+//  Created by Daniel Thorpe on 11/01/2014.
+//  Copyright (c) 2014 Daniel Thorpe. All rights reserved.
+//
+
+#ifndef DNTCoreData_DNTCoreData_h
+#define DNTCoreData_DNTCoreData_h
+
+#import "DNTCoreDataStack.h"
+#import "NSManagedObjectContext+DNTAdditions.h"
+
+#endif
